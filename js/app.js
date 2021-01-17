8f436947775ee3e05285bb637a10aaf8e881a564
khasan0102 <khasanabdukarimov1@gmail.com>
2021-01-17 14:57:25 +0500

    end Chat

---
+++ js/app.js
@@ -0,0 +1,70 @@
+let input = document.querySelector('#writeText');
+let inputUrl = document.querySelector('#writeUrl');
+let btn = document.querySelector('#btn');
+let btn1 = document.querySelector('#btn1');
+let imgBtn = document.querySelector('#imgBtn');
+let place = document.querySelector('#place');
+let main = document.querySelector('.chat-main');
+let date = new Date();
+let minut = date.getMinutes();
+let hours = date.getHours();
+
+function newElemet(element) {
+    return document.createElement(element);
+}
+
+function addMessage() {
+    let div = newElemet('div');
+    let time = newElemet('time');
+    let p = newElemet('p');
+    div.classList.add('message');
+    p.classList.add('message-text');
+    
+    main.appendChild(div)
+    div.appendChild(time);
+    div.appendChild(p);
+    p.textContent = input.value
+    time.innerText = hours + ":" + minut; 
+    p.style.maxWidth = "220px"
+    input.value = "";
+
+    main.scrollTop = main.scrollHeight;
+}
+function addImage(url) {
+    url = inputUrl;
+    let div = newElemet('div');
+    let img = newElemet('img');
+    let time = newElemet("time");
+    time.innerText = hours + ":" + minut; 
+    img.setAttribute("src", url.value);
+    img.setAttribute("alt", "Img message");
+    div.classList.add('message');
+    div.appendChild(time);
+    div.appendChild(img);
+    main.appendChild(div);
+    
+    btn1.classList.remove('btn1Act');
+    url.classList.remove('urlActive');
+    url.value = "";
+    main.scrollTop = main.scrollHeight;
+}
+
+btn.addEventListener('click', addMessage)
+btn1.addEventListener('click', addImage);
+
+input.addEventListener('keyup', function(event) {
+    if(event.keyCode == 13) {
+        addMessage();
+    }
+})
+
+imgBtn.addEventListener('click' , function()  {
+  inputUrl.classList.add('urlActive');
+  btn1.classList.add('btn1Act');
+})
+
+inputUrl.addEventListener('keyup', function(event) {
+    if(event.keyCode == 13) {
+        addImage(inputUrl);
+    }
+})
